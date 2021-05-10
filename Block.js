2b8e9e2a5998b896e4789420b3ea852881cb310a
ShreeyaSaini <74184402+ShreeyaSaini@users.noreply.github.com>
2021-05-10 23:18:01 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,30 @@
+class Block {
+    constructor(x,y) {
+
+        var options={
+            restitution: 0.8,
+            density: 1.0,
+            friction: 1.0
+        }
+
+        this.body = Bodies.rectangle(x,y,25,25,options);
+        this.visibility = 225;
+        World.add(world,this.body);
+    
+    } display() {
+        if(this.body.speed < 3) {
+        var pos = this.body.position;
+
+        fill("red");
+        rectMode(CENTER)
+        strokeWeight(1.5);
+        stroke("black");
+        rect(pos.x, pos.y,25,25);
+        } else {
+         World.remove(world, this.body);
+         push();
+         this.visibility = this.visibility - 5;
+         pop();
+        }
+    }
+}
